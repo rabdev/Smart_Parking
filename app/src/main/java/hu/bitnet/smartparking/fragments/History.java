6033e346a0a5e80007a1330c15477d56b9af9a69
nyulg <rabbitxdev@gmail.com>
2017-10-02 09:11:28 +0200

    history

--- app/src/main/java/hu/bitnet/smartparking/fragments/History.java
+++ app/src/main/java/hu/bitnet/smartparking/fragments/History.java
@@ -3,6 +3,8 @@ package hu.bitnet.smartparking.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,7 @@ import static android.view.KeyEvent.KEYCODE_BACK;
  */
 public class History extends Fragment {
 
+    RecyclerView history_rv;
 
     public History() {
         // Required empty public constructor
@@ -29,6 +32,10 @@ public class History extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View history = inflater.inflate(R.layout.fragment_history, container, false);
+        history_rv = (RecyclerView) history.findViewById(R.id.history_rv);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        history_rv.setLayoutManager(layoutManager);
+
         return history;
     }
 
